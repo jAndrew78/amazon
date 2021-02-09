7656990cd622482d24260ee831fee5b3830c1ccd
jAndrew78 <58049852+jAndrew78@users.noreply.github.com>
2021-02-09 17:56:30 -0600

    more initial tidying up

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+import Home from './components/Home';
 import './App.css';
 
+
 function App() {
-  return (
-    <div className="App">React App</div>
+    return (
+
+        <div className="app">
+
+            <Router>
+
+                <Switch>
+
+                    <Route path="/">
+                        <Home />
+                    </Route>
+
+                </Switch>
+            </Router>
+        </div>
   );
 }
+
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import { render } from 'react-dom';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './index.css';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+render(<App />, document.getElementById('root'));
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+// tutorial didn't say to change the following, so I'm keeping it for now and commenting out
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+// import App from './App';
+// import reportWebVitals from './reportWebVitals';
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+
+// reportWebVitals();
