a1c82d78d0f544d1f2e61e8a348e00c7496d9de3
jAndrew78 <58049852+jAndrew78@users.noreply.github.com>
2021-02-12 16:25:30 -0600

    app: moved bootstrap import here, Navbar: styling, nowrap, Home: styling, carousel

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
+import 'bootstrap/dist/css/bootstrap.css';
+import './App.css';
+
 import Home from './components/Home.jsx';
 import Navbar from './components/Navbar.jsx';
 import Footer from './components/Footer.jsx';
-import './App.css';
 
 
 function App() {
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import 'bootstrap/dist/css/bootstrap.css';
 import '../styles/Home.css';
 
 // COMPONENTS
@@ -8,53 +7,51 @@ import Product from './Product';
 // IMAGES
 import Prime from './utils/images/hero/prime-video.jpg';
 import Audible from './utils/images/hero/audible.jpg';
-// import Alexa from './utils/images/hero/alexa-laptop.jpg';
 import Certified from './utils/images/hero/certified-refurbished.jpg';
+// import Alexa from './utils/images/hero/alexa-laptop.jpg';
 
 
-function Home() {    
+function Home() {
     return (
         <div className="home">
 
             <div className="home-container">
-                {/* <img
-                    className="home-image" alt="home hero img"
-                    // src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg"
-                    src={Prime}
-                /> */}
 
                 {/* HERO CAROUSEL */}
-                <div id="carouselExampleControls" className="carousel slide carousel-fade" data-bs-ride="carousel">
-                    <div className="carousel-inner">
-                        
-                        <div className="carousel-item active" data-bs-interval="6000">
-                            <img className="d-block w-100" src={Prime} alt="First slide" />
+                <div className="carousel-container">
+
+                    <div id="heroCarousel" className="carousel slide carousel-fade" data-bs-ride="carousel">
+                        <div className="carousel-inner">
+                            
+                            <div className="carousel-item active" data-bs-interval="6000">
+                                <img className="d-block w-100" src={Prime} alt="1st Slide" />
+                            </div>
+
+                            <div className="carousel-item" data-bs-interval="6000">
+                                <img className="d-block w-100" src={Certified} alt="2nd Slide" />
+                            </div>
+
+                            <div className="carousel-item" data-bs-interval="6000">
+                                <img className="d-block w-100" src={Audible} alt="3rd Slide" />
+                            </div>
+
+                            {/* <div className="carousel-item" data-bs-interval="6000">
+                                <img className="d-block w-100" src={Alexa} alt="4th Slide" />
+                            </div> */}
+                            
                         </div>
 
-                        <div className="carousel-item" data-bs-interval="6000">
-                            <img className="d-block w-100" src={Audible} alt="Second slide" />
-                        </div>
+                        <a className="carousel-control-prev" href="#heroCarousel" role="button" data-bs-slide="prev">
+                            <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span className="sr-only">Previous</span>
+                        </a>
 
-                        {/* <div className="carousel-item" data-bs-interval="6000">
-                            <img className="d-block w-100" src={Alexa} alt="Third slide" />
-                        </div> */}
-                        
-                        <div className="carousel-item" data-bs-interval="6000">
-                            <img className="d-block w-100" src={Certified} alt="Third slide" />
-                        </div>
+                        <a className="carousel-control-next" href="#heroCarousel" role="button" data-bs-slide="next">
+                            <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span className="sr-only">Next</span>
+                        </a>
 
                     </div>
-
-                    <a className="carousel-control-prev" href="#carouselExampleControls" role="button" data-bs-slide="prev">
-                        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-                        <span className="sr-only">Previous</span>
-                    </a>
-
-                    <a className="carousel-control-next" href="#carouselExampleControls" role="button" data-bs-slide="next">
-                        <span className="carousel-control-next-icon" aria-hidden="true"></span>
-                        <span className="sr-only">Next</span>
-                    </a>
-
                 </div>
 
                 {/* PRODUCT ROWS */}
