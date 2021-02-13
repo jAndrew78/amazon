7132fa7b4179fd58cffe0977b6401cdf9a5bd553
jAndrew78 <58049852+jAndrew78@users.noreply.github.com>
2021-02-13 11:57:49 -0600

    wrap App in StateProvider with initialState and reducer as props

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
+import React from 'react';
 import { render } from 'react-dom';
+import reducer, { initialState } from './components/utils/context/reducer';
+import { StateProvider } from './components/utils/context/StateProvider';
+
 import App from './App.jsx';
 import './index.css';
 
-render(<App />, document.getElementById('root'));
+render(
+    <StateProvider initialState={initialState} reducer={reducer}>
+        <App />
+    </StateProvider>,
+    document.getElementById('root')
+);
 
 
 // tutorial didn't say to change the following, so I'm keeping it for now and commenting out
