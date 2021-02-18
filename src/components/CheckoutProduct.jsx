c1e35889f4588d9ae18503a6d43aad1eb86c02d6
jAndrew78 <58049852+jAndrew78@users.noreply.github.com>
2021-02-18 14:03:43 -0600

    renamed Checkout to CheckoutProduct, created new Checkout component and css from template, fixed subtotal button styling

---
+++ src/components/CheckoutProduct.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import { useStateValue } from './utils/context/StateProvider';
+import '../styles/CheckoutProduct.css';
+
+function CheckoutProduct({ id, image, title, price, rating, hideButton }) {
+
+    const [{ cart }, dispatch] = useStateValue();
+
+    const removeFromCart = () => {
+        dispatch({
+            type: 'REMOVE_FROM_CART',
+            id: id,
+        })
+    }
+
+    return (
+        <div className="checkout-product">
+            <image className="checkout-product-image" src={image} />
+
+            <div className="checkout-product-info">
+
+                <p className="checkout-product-title">{title}</p>
+                
+                <p className="checkout-product-price">
+                    <small>$</small>
+                    <strong>{price}</strong>
+                </p>
+
+
+                <div className="checkout-product-rating">
+                    {Array(rating)
+                        .fill()
+                        .map((_, i) => (
+                            <p key={i} className="checkout-product-rating-stars">⭐</p>
+                            ))}
+                </div>
+
+                {!hideButton && (
+                    <button className="checkout-product-button" onClick={removeFromCart}>Remove From Cart</button>
+                    )}
+
+            </div>
+        </div>
+    )
+}
+
+export default CheckoutProduct;
