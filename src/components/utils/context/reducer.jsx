da93afc708271415f53dfac2929d952231d60306
jAndrew78 <58049852+jAndrew78@users.noreply.github.com>
2021-02-18 13:03:16 -0600

    added cases EMPTY_CART and REMOVE_FROM_CART

--- src/components/utils/context/reducer.jsx
+++ src/components/utils/context/reducer.jsx
@@ -13,8 +13,35 @@ const reducer = (state, action) => {
         case "ADD_TO_CART":
             return {
                 ...state,
-                cart: [...state.cart, action.item],
+                cart: [...state.cart, action.item]
+            };
+        
+        case 'EMPTY_CART': 
+            return {
+                ...state,
+                cart: []
+            };
+
+        case "REMOVE_FROM_CART":
+            
+            const index = state.cart.findIndex(
+                cartItem => cartItem.id === action.id
+            )
+            let newCart = [...state.cart]
+
+            if (index >= 0) {
+                newCart.splice(index, 1)
+            } else {
+                console.warn(
+                    `Product (id: {action.id}) is not in the cart. Cannot remove.`
+                )
             }
+
+            return {
+                ...state,
+                user: action.user
+            }
+
         default:
             return state;
     }
