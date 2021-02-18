a19e62977fd178a7196184c67127982641381b3b
jAndrew78 <58049852+jAndrew78@users.noreply.github.com>
2021-02-18 10:57:08 -0600

    basic components from template plus empty css files

---
+++ src/components/Checkout.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { useStateValue } from './utils/context/StateProvider';
+import '../styles/Checkout.css';
+
+function Checkout() {
+    return (
+        <div className="checkout">Checkout</div>
+    )
+}
+
+export default Checkout;
---
+++ src/components/Subtotal.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { useStateValue } from './utils/context/StateProvider';
+import '../styles/Subtotal.css';
+
+function Subtotal() {
+    return (
+        <div className="subtotal">Subtotal</div>
+    )
+}
+
+export default Subtotal;
