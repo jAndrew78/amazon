787570c26600a12b7b474e4b9e2d0bc29bdbc110
jAndrew78 <58049852+jAndrew78@users.noreply.github.com>
2021-02-18 14:31:24 -0600

    fleshed out Checkout Component, still some work to do

---
+++ src/components/Checkout.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import Subtotal from './Subtotal';
+import CheckoutProduct from './CheckoutProduct';
+import { useStateValue } from './utils/context/StateProvider';
+import '../styles/Checkout.css';
+
+import OceanCard from './utils/images/banner-ads/ocean-card.jpg'
+
+
+function Checkout() {
+    const [{ cart, user }, dispatch] = useStateValue();
+
+    return (
+        <div className='checkout'>
+            <div className="checkout-left">
+
+                <img className="checkout-ad" src={OceanCard} alt="banner ad"/>
+                
+                <div>
+                    <h3>Hello, {user?.email}</h3>
+                    <h2 className="checkout-title">Your Shopping Cart</h2>
+
+                    {cart.map(item => (
+                        <CheckoutProduct 
+                        id={item.id}
+                        title={item.title}
+                        image={item.image}
+                        price={item.price}
+                        rating={item.rating}
+                        />
+                        ))}
+                </div>
+                
+            </div>
+
+            <div className="checkout-right">
+                <Subtotal />
+            </div>
+        </div>
+    )
+}
+
+
+export default Checkout;
