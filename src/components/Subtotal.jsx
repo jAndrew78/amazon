1c3d8eda336d5b10802d4087cb3fea442475ac9f
jAndrew78 <58049852+jAndrew78@users.noreply.github.com>
2021-02-18 13:43:35 -0600

    fleshing out subtotal component

--- src/components/Subtotal.jsx
+++ src/components/Subtotal.jsx
@@ -1,10 +1,38 @@
 import React from 'react';
+import CurrencyFormat from 'react-currency-format';
+import { useHistory } from 'react-router-dom';
 import { useStateValue } from './utils/context/StateProvider';
+import { getCartTotal } from './utils/context/reducer';
 import '../styles/Subtotal.css';
 
+
 function Subtotal() {
+    const history = useHistory();
+    const [{ cart }, dispatch] = useStateValue();
+
     return (
-        <div className="subtotal">Subtotal</div>
+        <div className="subtotal">
+            <CurrencyFormat
+                renderText={value => (
+                    <>
+                        <p>
+                            {/* ??? */}
+                            Subtotal ({cart.length} items): <strong>{value}</strong>
+                        </p>
+                        <small className="subtotal-gift">
+                            <input type="checkbox" /> This order contains a gift
+                        </small>
+                    </>
+                )}
+                decimalScale={2}
+                value={getCartTotal(cart)} // ???
+                displayType={"text"}
+                thousandSeparator={true}
+                prefix={"$"}
+            />
+
+            <button className="subtotal-button">Proceed to Checkout</button>
+        </div>
     )
 }
 
