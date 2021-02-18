cd0e51d78b27f4758f011146d130ec6abdf9aceb
jAndrew78 <58049852+jAndrew78@users.noreply.github.com>
2021-02-18 10:09:58 -0600

    useEffect checking for login and setting SET_USER state

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect }from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import 'bootstrap/dist/css/bootstrap.css';
@@ -8,9 +8,30 @@ import Home from './components/Home.jsx';
 import Navbar from './components/Navbar.jsx';
 import Footer from './components/Footer.jsx';
 import Login from './components/Login.jsx';
+import { auth } from './firebase/firebase';
 
 
 function App() {
+
+    useEffect(() => {
+        
+        auth.onAuthStateChanged(authUser => {
+            console.log("the user is >>> ", authUser)
+
+            if (authUser) {
+                dispatchEvent({
+                    type: "SET_USER",
+                    user: authUser,
+                })
+            } else {
+                dispatchEvent({
+                    type: "SET_USER",
+                    user: null,
+                })
+            }
+        })
+    }, []);
+
     return (
 
         <div className="app">
