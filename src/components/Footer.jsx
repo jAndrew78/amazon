8998a0e4576dc5e4143498c334547cd23c51e241
jAndrew78 <58049852+jAndrew78@users.noreply.github.com>
2021-03-11 21:05:43 -0600

    footer 1 - just need to link to somewhere

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -12,10 +12,63 @@ const scrollToTop = () => {
 function Footer() {
     return (
         <div className="footer">
+
             <div className='footer-link-to-top' onClick={scrollToTop}>Back to top</div>
-            <div className='footer-1'></div>
+
+            <div className='footer-1'>
+                <div className="footer-1-row">
+                    <div className="footer-1-header">Get to Know Us</div>
+                    <div className="footer-1-link">Careers</div>
+                    <div className="footer-1-link">Blog</div>
+                    <div className="footer-1-link">About nozamA</div>
+                    <div className="footer-1-link">Sustainability</div>
+                    <div className="footer-1-link">Press Center</div>
+                    <div className="footer-1-link">Investor Relations</div>
+                    <div className="footer-1-link">Amazon Devices</div>
+                    <div className="footer-1-link">Amazon Tours</div>
+                </div>
+
+                <div className="footer-1-row">
+                    <div className="footer-1-header">Make Money with Us</div>
+                    <div className="footer-1-link">Sell products on nozamA</div>
+                    <div className="footer-1-link">Sell apps on nozamA</div>
+                    <div className="footer-1-link">Become an Affiliate</div>
+                    <div className="footer-1-link">Advertise Your Products</div>
+                    <div className="footer-1-link">Self-Publish with Us</div>
+                    <div className="footer-1-link">Host a nozamA Hub</div>
+                    <div className="footer-1-link">Something Else to Make it all Even</div>
+                    <div className="footer-1-link">&#8250; See More Make Money with Us</div>
+                </div>
+
+                <div className="footer-1-row">
+                    <div className="footer-1-header">nozamA Payment Products</div>
+                    <div className="footer-1-link">nozamA Rewards Visa Signature Cards</div>
+                    <div className="footer-1-link">moc.nozamA Store Card</div>
+                    <div className="footer-1-link">nozamA Business Card</div>
+                    <div className="footer-1-link">nozamA Business Line of Credit</div>
+                    <div className="footer-1-link">Shop with Points</div>
+                    <div className="footer-1-link">Credit Card Marketplace</div>
+                    <div className="footer-1-link">Reload Your Balance</div>
+                    <div className="footer-1-link">nozamA Currency Calculator</div>
+                </div>
+
+                <div className="footer-1-row">
+                    <div className="footer-1-header">Let Us Help You</div>
+                    <div className="footer-1-link">Your Account</div>
+                    <div className="footer-1-link">Your Orders</div>
+                    <div className="footer-1-link">Shipping Rates & Policies</div>
+                    <div className="footer-1-link">nozamA Prime</div>
+                    <div className="footer-1-link">Returns & Replacements</div>
+                    <div className="footer-1-link">Manage Your Content & Devices</div>
+                    <div className="footer-1-link">nozamA Assistant</div>
+                    <div className="footer-1-link">Help</div>
+                </div>
+            </div>
+
             <div className='footer-2'></div>
+
             <div className='footer-3'></div>
+
         </div>
         
     )
