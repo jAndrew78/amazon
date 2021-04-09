fac02aa578e4daf435855c844af9176eabaf67db
jAndrew78 <58049852+jAndrew78@users.noreply.github.com>
2021-04-09 13:50:19 -0500

    npm i express cors stripe

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,12 @@
 const functions = require("firebase-functions");
+const express = require('express');
+const cors = require('cors');
 
+
+
+
+
+// *** READ LATER ***
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
